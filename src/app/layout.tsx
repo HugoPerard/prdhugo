eeb293c8d2b04333f4e4b866b7ff694045cbf33e
Hugo Pérard <hugo.perard@bearstudio.fr>
2026-03-03 00:31:24 +0700

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,15 @@ export const metadata: Metadata = {
   description:
     "Personal site of Hugo Pérard, lead front-end developer based in Rouen, France.",
   icons: {
-    icon: [{ url: "/favicon.svg", type: "image/svg+xml" }],
-    apple: "/favicon.svg",
+    icon: [
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", type: "image/png", sizes: "180x180" },
+    ],
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
